0b4bd9fc258d0992391ae4871f6c3e19a0204aab
rchumpon <vinzen_victor@hotmail.com>
2025-09-08 01:32:33 +1000

    Create Q2.1_objects.js

---
+++ Q2.1_objects.js
@@ -0,0 +1,11 @@
+// File: Q2.1_objects.js
+// Description: File to show how to build an object called myMovie.
+// Author: Roongroj Chumpon
+// Date: 08/09/25
+
+let myMovie = {
+  'Title' : 'The longest day',
+  'Year' : '1964'
+};
+
+console.log(myMovie);
