808c7de4b3c191bbecd7ae3b1003619b213f504a
rchumpon <vinzen_victor@hotmail.com>
2025-09-08 02:44:53 +1000

    Create Q2.3_changeObjects.js

---
+++ Q2.3_changeObjects.js
@@ -0,0 +1,20 @@
+// File: Q2.3_addObjects.js
+// Description: File to show how to change the data of the properties to the object called myMovie.
+// Author: Roongroj Chumpon
+// Date: 08/09/25
+
+
+let myMovie = {
+  'Title' : 'The longest day',
+  'Year' : 1964
+};
+console.log(myMovie);
+
+myMovie.Rating = 5;
+myMovie.Summary = 'World War II movie about the Normandy landings';
+console.log(myMovie);
+
+// Change data of the properties
+myMovie.Rating = 4;
+myMovie.Year = 1962;
+console.log(myMovie);
