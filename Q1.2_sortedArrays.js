9a1675cf44ee8815dea74f97a8ea223b40ac8bc0
rchumpon <vinzen_victor@hotmail.com>
2025-10-17 10:27:13 +1100

    added Q.1.2_sortedArrays.html

--- Q1.2_sortedArrays.js
+++ Q1.2_sortedArrays.js
@@ -2,6 +2,7 @@
 // Desciption: File to sort an array in ascending order
 // Author: Roongroj Chumpon
 // Date: 05/09/25
+// Version: 1.0.0
 
 
 // Q1.2  Sort the array created in Q1.1 above in “ascending” order (ascending is smallest to largest)
@@ -11,4 +12,5 @@ const myArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
 
 // Fix the incorrect sorting we can use a method as a parameter
 const sortedNumber = myArray.sort(function (a, b) {return a - b;});
+// Output the result.
 console.log(sortedNumber);
