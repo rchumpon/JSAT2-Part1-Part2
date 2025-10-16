64a0e7787e55b2b41b7be0f1117e24e08601a1dc
rchumpon <vinzen_victor@hotmail.com>
2025-10-17 10:18:19 +1100

    added Q1.1_array.html

--- Q1.1_array.js
+++ Q1.1_array.js
@@ -2,6 +2,7 @@
 // Description: File to show an array with numbers and print the array to the console
 // Author: Roongroj Chumpon
 // Date: 05/09/25
+// Version: 1.0.0
 
 // Q1.1 Build an array with the following numbers in JavaScript and print the array in the console: 11, 5, 8, 3, 25, 16, 31, 45, 14, 20
 
