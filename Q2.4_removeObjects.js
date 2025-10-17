73b1eb62659587ef74e5e39f17d41e5f5f915a98
rchumpon <vinzen_victor@hotmail.com>
2025-10-17 13:15:00 +1100

    added Q2.4_removeObjects.html

--- Q2.4_removeObjects.js
+++ Q2.4_removeObjects.js
@@ -2,16 +2,19 @@
 // Description: File to show how to remove the data of the properties
 // Author: Roongroj Chumpon
 // Date: 08/09/25
+// Version: 1.0.0
 
+// Create an object called myMovie with four properties.
 let myMovie = {
   'Title' : 'The longest day',
   'Year' : 1964,
   'Rating' : 5,
   'Summary' : "World War II movie about the Normandy landings"
 };
-
+// Output the object.
 console.log(myMovie);
 
-//Remove the properties
+// Use the delete operator to remove a property.
 delete myMovie.Summary;
+// Display the updated object.
 console.log(myMovie);
