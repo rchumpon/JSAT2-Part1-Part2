6013bdeed535f4b8822803040680e6b21c2ba63e
rchumpon <vinzen_victor@hotmail.com>
2025-10-17 12:18:54 +1100

    added Q2.1_objects.html

--- Q2.1_objects.js
+++ Q2.1_objects.js
@@ -2,10 +2,13 @@
 // Description: File to show how to build an object called myMovie.
 // Author: Roongroj Chumpon
 // Date: 08/09/25
+// Version: 1.0.0
 
+// Create an object named myMovie with two properties: Title and Year.
 let myMovie = {
-  'Title' : 'The longest day',
-  'Year' : 1964
+  'Title' : 'The longest day', // Property 'Title' with a string value.
+  'Year' : 1964 // Property 'Year' with a numeric value.
 };
-
+// Output the object.
 console.log(myMovie);
+
