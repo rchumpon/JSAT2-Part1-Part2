1d4cc04847d5c61c1a4214777eda72b765c76926
rchumpon <vinzen_victor@hotmail.com>
2025-10-17 12:29:24 +1100

    added Q2.2_addObjects.html

--- Q2.2_addObjects.js
+++ Q2.2_addObjects.js
@@ -2,7 +2,9 @@
 // Description: File to show how to add properties to the object called myMovie.
 // Author: Roongroj Chumpon
 // Date: 08/09/25
+// Version: 1.0.0
 
+// Create an object called myMovie with two properties: Title and Year.
 let myMovie = {
   'Title' : 'The longest day',
   'Year' : 1964
@@ -11,4 +13,5 @@ console.log(myMovie);
 // Adding data of the properites
 myMovie.Rating = 5;
 myMovie.Summary = 'World War II movie about the Normandy landings';
+// Display the updated object.
 console.log(myMovie);
